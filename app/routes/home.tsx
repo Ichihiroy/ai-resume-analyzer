ccc7afc88a86f7b415910ad70782633cad93cf32
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-08-27 18:41:08 +0400

    Add resume data structure and implement ResumeCard component for displaying resumes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { resumes } from "constans";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,10 +18,15 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Welcome to the AI Resume Analyzer</h1>
           <h2>Review your submissions with our AI-powered tool!</h2>
         </div>
+        <section className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </section>
       </section>
     </main>
   );
