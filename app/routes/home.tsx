c8cfbe8f6122f30b29e5e37c2d98294e6862de0a
Yunis Quliyev <realyanisquliyev@gmail.com>
2025-08-27 15:17:16 +0400

    Refactor Home component and remove unused Welcome component and logos

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,26 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Resume Analyzer" },
+    {
+      name: "description",
+      content: "Analyze your resume with our AI-powered tool!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Welcome to the AI Resume Analyzer</h1>
+          <h2>Review your submissions with our AI-powered tool!</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
